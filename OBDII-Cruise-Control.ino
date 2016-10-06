8ce9e9c796333ba2982a2978eca8c12c0919b6d2
Rogério Carvalho Schneider <stockrt@gmail.com>
2016-10-06 12:25:09 -0300

    Using two software serial ports

--- OBDII-Cruise-Control.ino
+++ OBDII-Cruise-Control.ino
@@ -4,7 +4,8 @@
 
   Hardware:
   - Arduino Uno R3
-  - HC-05 BlueTooth module (zs-040)
+  - HC-05 BlueTooth module (zs-040) (connects to OBDII)
+  - HC-06 BlueTooth module (JY-MCU) (connects to a serial terminal for monitoring and command)
   - ELM327 OBDII BlueTooth adapter (OBD to RS232 interpreter v1.5)
   - Servo motor connected to pull car's throtle (TowerPro MG956R 12kg)
   - Switch button on throtle
@@ -26,72 +27,128 @@
 #include <SoftwareSerial.h>
 #include <Timer.h>
 
-#define RxD 7 // Arduino pin connected to Tx of HC-05
-#define TxD 8 // Arduino pin connected to Rx of HC-05
+#define OBD_RxD 2 // Arduino pin connected to Tx of HC-05 (OBD)
+#define OBD_TxD 3 // Arduino pin connected to Rx of HC-05
+#define MONIT_RxD 4 // Arduino pin connected to Tx of HC-06 (MONIT)
+#define MONIT_TxD 5 // Arduino pin connected to Rx of HC-06
 
-SoftwareSerial btSerial(RxD, TxD);
+int targetVelocity = 0;
+
+SoftwareSerial btSerial(OBD_RxD, OBD_TxD);
+SoftwareSerial btMonitSerial(MONIT_RxD, MONIT_TxD);
 Timer t;
 
 void setup() {
   // Serial config
   Serial.begin(38400);
   btSerial.begin(38400);
+  btMonitSerial.begin(38400);
 
-  Serial.println("Initializing...");
+  serialPrintln("Initializing...");
 
-  // HC-05 communication
-  pinMode(RxD, INPUT);
-  pinMode(TxD, OUTPUT);
+  // HC-05 communication to OBDII
+  pinMode(OBD_RxD, INPUT);
+  pinMode(OBD_TxD, OUTPUT);
+
+  // HC-06 communication to serial monitor
+  pinMode(MONIT_RxD, INPUT);
+  pinMode(MONIT_TxD, OUTPUT);
 
   // BlueTooth connection should be estabilished automatically if we have configured HC-05 correctly
   waitBT();
 
   t.every(500, cruiseControl);
 
-  Serial.println("System initialized");
+  serialPrintln("System initialized");
 }
 
 // Wait until we can communicate with OBDII adapter via HC-05 BlueTooth module
 void waitBT() {
-  String recv;
+  String btRecv;
   int count;
 
+  btSerial.listen();
   while (true) {
     btSerial.flush();
     btSerial.println("ATZ");
 
     count = 0;
     while (count < 200) { // Wait response from OBDII for two seconds at most
-      if (btSerial.available()) break;
+      if (btSerial.available()) {
+        break;
+      }
       count++;
       delay(10);
     }
 
     if (btSerial.available()) {
-      recv = btSerial.readString();
+      btRecv = btSerial.readString();
 
-      if (recv.indexOf("ELM327") != -1) {
+      if (btRecv.indexOf("ELM327") != -1) {
         btSerial.flush();
-        Serial.println("OBDII ready");
+        serialPrintln("OBDII ready");
         break;
       } else {
-        Serial.println("OBDII not ready yet (waiting 1s to retry)");
+        serialPrintln("OBDII not ready yet (waiting 1s to retry)");
       }
     } else {
-      Serial.println("No response from OBDII (waiting 1s to retry)");
+      serialPrintln("No response from OBDII (waiting 1s to retry)");
     }
 
     delay(1000);
   }
 }
 
+void serialWrite(char msg) {
+  Serial.write(msg);
+  btMonitSerial.write(msg);
+}
+
+void serialPrint(String msg) {
+  Serial.print(msg);
+  btMonitSerial.print(msg);
+}
+
+void serialPrintln(String msg) {
+  Serial.println(msg);
+  btMonitSerial.println(msg);
+}
+
 void cruiseControl() {
-  Serial.println("Cruising");
+  serialPrintln("Cruising");
 }
 
 void loop() {
   t.update();
 
-  if (btSerial.available()) Serial.write(btSerial.read());
-  if (Serial.available()) btSerial.write(Serial.read());
+  // Bridge mode (for debugging)
+  //btSerial.listen();
+  //if (btSerial.available()) serialWrite(btSerial.read());
+  //if (Serial.available()) btSerial.write(Serial.read());
+  //btMonitSerial.listen();
+  //if (btMonitSerial.available()) btSerial.write(btMonitSerial.read());
+
+  String serialRecv = "";
+
+  // Accept commands from serial terminals
+  btMonitSerial.listen(); // HC-06 port can be read now
+  if (Serial.available()) {
+    serialRecv = Serial.readString();
+  } else if (btMonitSerial.available()) {
+    serialRecv = btMonitSerial.readString();
+  }
+  if (serialRecv.startsWith("v=")) {
+    targetVelocity = serialRecv.substring(2).toInt();
+    serialPrint("ACK command (target velocity set): ");
+    serialPrintln(serialRecv);
+  } else {
+    serialPrint("Unknown command: ");
+    serialPrintln(serialRecv);
+  }
+  btSerial.listen(); // HC-05 port can be can read
+
+  serialPrint("Target velocity: ");
+  serialPrintln(String(targetVelocity));
+
+  delay(1000);
 }
