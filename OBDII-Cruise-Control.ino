3dddba1454c1ca103d9f102b5e8eb671bdcd65b5
Rogério Carvalho Schneider <stockrt@gmail.com>
2016-10-19 17:55:20 -0200

    Software disclaimer and previous works

--- OBDII-Cruise-Control.ino
+++ OBDII-Cruise-Control.ino
@@ -5,6 +5,17 @@
   It can also keep RPM as set by the user via Serial, just for fun.
   Commands issued via Serial have precedence over speed set via RF control.
 
+  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS
+  OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+  OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+  OF SUCH DAMAGE.
+
   Commands (via Serial port):
   - s=80    set target SPEED to 80
   - r=1000  set target RPM to 1000
@@ -37,6 +48,9 @@
   - https://www.youtube.com/watch?v=O-OqgFE9SD4
   - https://www.youtube.com/watch?v=UR0hOmjaHp0
   - https://www.youtube.com/watch?v=XfAt6hNV8XM
+
+  Previous works:
+  - https://github.com/simoesusp/CarPuter
 
   Author: Rogério Carvalho Schneider <stockrt@gmail.com>
 
